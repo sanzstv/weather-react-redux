0a727b89383e2d8e7a407dd8777caa42b2f0b3a7
sanzstv <steviesanz@comcast.net>
2018-01-22 10:12:10 -0800

    security\

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = "";
+const API_KEY = "5acb97b62ff03f7c2ed9bdafb1fd7b35";
 const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeatherData(city){
